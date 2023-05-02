89ae41332c3e6f5486a27cd71e37199474802edd
npayton77 <nick@paytonsplace.net>
2023-05-02 13:38:25 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -14,7 +14,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. test test test. 
+  Welcome to ${PREFIX}'s app. testing 321..... 
   <!-- END -->
   
   </div>
